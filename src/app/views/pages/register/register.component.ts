9bdffa159ff0f8a555da9b9cb2ef7a24bde2382b
Lucas Gabriel <lucasmoreira.gabriel2004@gmail.com>
2024-02-23 16:27:04 -0300

    add functionally register page

--- src/app/views/pages/register/register.component.ts
+++ src/app/views/pages/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpRequest } from 'src/app/services/http-request.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +10,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  myForm: FormGroup;
+  public isRepeatedPassEquals: boolean = false;
 
-  constructor() { }
+  
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder, 
+    private httpService: HttpRequest,
+    private alert: MatSnackBar
+  ) {
+    this.myForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['',[Validators.required, Validators.email]],
+      passwd: ['', [Validators.required]],
+      repeatedPass: ['', Validators.required]
+    })
+  }
 
+  get f() {
+    return this.myForm.controls;
+  }
+
+  onRegister() {
+    if (!this.myForm.valid) {
+      this.myForm.markAllAsTouched();
+      return
+    }
+
+    this.httpService.createUser(this.myForm.value).subscribe(
+      {
+        next: (v) => console.log(v),
+        error: (e) => {
+          this.alert.open('An error occurred!')
+          console.error(e)
+        },
+        complete: () => {
+          this.alert.open('User created!')
+          this.router.navigate(['/dashboard']);
+        }
+      }
+    )
+  }
+
+  onChangePass() {
+    const { passwd, repeatedPass } = this.f;
+
+    if (passwd.value === repeatedPass.value)
+      this.isRepeatedPassEquals = true;
+    else
+      this.isRepeatedPassEquals = false;
+  }
 }
